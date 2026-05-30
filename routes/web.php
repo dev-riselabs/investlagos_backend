06a4e5f4e64b079c3b41cfc380ba1965662e05e5
tsuccess <taofeeq.muhammad22@gmail.com>
2026-05-30 05:41:03 +0100

    update backend route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function (): JsonResponse {
+    return response()->json([
+        'message' => 'Invest Lagos',
+        'data' => [
+            'status' => 'ok',
+            'app' => config('app.name'),
+            'health' => url('/api/v1/health'),
+        ],
+    ]);
 });
